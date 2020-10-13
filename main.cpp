bbdf7fa350c88df799bd23ad40489743061cdb44
baseoleph <baseoleph@gmail.com>
2020-10-14 02:37:23 +0300

    set window in center of screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    //Set window in center of screen
+    QDesktopWidget *desktop = QApplication::desktop();
+    int x = (desktop->width() - w.width()) / 2;
+    int y = (desktop->height() - w.height()) / 3;
+    w.move(x, y);
+
     w.show();
     return a.exec();
 }
